b9b0c1055fb0ac320d8e1eceab6fa1f320ad3a37
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-14 19:22:23 -0300

    Update script.js

--- api/script.js
+++ api/script.js
@@ -1,4 +1,10 @@
-export default function handler(req, res) {
-  const luaResponse = `return "loadstring(game:HttpGet(\\"https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject\\"))()"`;
-  res.status(200).json({ script: luaResponse });
-}
+const express = require('express');
+const app = express();
+
+app.get('/api/script', (req, res) => {
+  const code = `loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()`;
+  res.type('text/plain');
+  res.send(code);
+});
+
+app.listen(3000);
