ce3fc255cb6f1b3f8fd47dd8910494f534208556
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-14 19:17:49 -0300

    Update api.lua

--- api/api.lua
+++ api/api.lua
@@ -1,1 +1,18 @@
+-- api.lua
+-- Script que gera a string pra resposta da API
+
+local script_lua = [[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()
+]]
+
+-- Função pra escapar caracteres especiais na string
+local function escape_str(s)
+    return s:gsub("\\", "\\\\"):gsub("\"", "\\\""):gsub("\n", "\\n")
+end
+
+-- Monta o return com a string escapada
+local res = 'return "' .. escape_str(script_lua) .. '"'
+
+print(res) -- só pra debug, pode tirar depois
+
+return res
