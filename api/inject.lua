47f732325a75f8dc210eb9b2be84bc63b7a8d8dc
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-14 19:54:18 -0300

    Update inject.lua

--- api/inject.lua
+++ api/inject.lua
@@ -1,1 +1,4 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.send(`loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()`);
+  }
