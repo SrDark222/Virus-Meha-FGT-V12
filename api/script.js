b9434a38745aebc9a1c4aede820172b8aedc0947
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-14 19:45:42 -0300

    Update script.js

--- api/script.js
+++ api/script.js
@@ -1,13 +1,16 @@
-export async function GET() {
-  const fs = require('fs');
-  const path = require('path');
+// api/script.js
 
+import fs from 'fs/promises';
+import path from 'path';
+
+export async function GET(req, res) {
   const filePath = path.join(process.cwd(), 'api', 'inject.lua');
-  const code = fs.readFileSync(filePath, 'utf-8');
-
+  const code = await fs.readFile(filePath, 'utf-8');
   return new Response(code, {
+    status: 200,
     headers: {
-      'Content-Type': 'text/plain'
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*'
     }
   });
 }
